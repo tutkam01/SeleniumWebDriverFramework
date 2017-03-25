73d35b4367f91454a63463a86300b1be1ee36c37
Mateusz Tutka <mateusz.tutka@gmail.com>
2017-03-25 14:20:50 +0100

    Establisihing maven project

---
+++ src/main/java/tutka/mateusz/selenium/App.java
@@ -0,0 +1,13 @@
+package tutka.mateusz.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
